8eb789c09ca2e22cda628925b040bbe466303434
Stefaan <safwebdev@gmail.com>
2021-11-03 11:24:12 +0800

    Setup basic Components & Layout

---
+++ src/Components/Details/styles.js
@@ -0,0 +1,6 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles(() => ({
+  income: { border: "10px solid rgba(0,255,0,0.5)" },
+  expense: { border: "10px solid rgba(255,0,0,0.5)" },
+}));
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,35 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import useStyles from "./styles";
+const Main = () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance $100
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
+        >
+          Try saying: Add income for $100 in Category Salary for Monday...
+        </Typography>
+      </CardContent>
+      <CardContent className={classes.cardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}></Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Main;
