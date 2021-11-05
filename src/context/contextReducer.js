6876df9a6ceb185022781ec1bf119038967686eb
Stefaan <safwebdev@gmail.com>
2021-11-05 08:50:19 +0800

    localStorage

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -4,8 +4,10 @@ const contextReducer = (state, action) => {
   switch (action.type) {
     case "DELETE_TRANSACTION":
       transactions = state.filter((t) => t.id !== action.payload);
+      localStorage.setItem("transactions", JSON.stringify(transactions));
       return transactions;
     case "ADD_TRANSACTION":
+      localStorage.setItem("transactions", JSON.stringify(transactions));
       transactions = [action.payload, ...state];
       return transactions;
 
